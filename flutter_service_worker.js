41f039ddfa5e5f7b8788d625e119381ec5b83e86
Zuo Wang <zwang1600@gmail.com>
2021-11-29 20:40:40 -0800

    updated email

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "281f2a7b1646c3dea6feea25f04fa556",
-"index.html": "7ae7e8c9e180699dad8b86df113b0b9d",
-"/": "7ae7e8c9e180699dad8b86df113b0b9d",
-"main.dart.js": "eddcaa4f719e3f1a69f4ad02b8f47e52",
+"index.html": "2707eccd1c42ea4c846094fc6ebd2ce6",
+"/": "2707eccd1c42ea4c846094fc6ebd2ce6",
+"main.dart.js": "d30fd67e8fc176aab5a4016277ec1d85",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
