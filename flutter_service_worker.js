79a7e85f69cc2888ea625f20400d7c17cfb2ca46
Zuo Wang <zwang1600@gmail.com>
2021-12-03 13:15:49 -0800

    added slivers

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "281f2a7b1646c3dea6feea25f04fa556",
-"index.html": "89386b748a6041bd4ff511ebbb620bb2",
-"/": "89386b748a6041bd4ff511ebbb620bb2",
-"main.dart.js": "b8740480c273ab2513fc42e848afafe0",
+"index.html": "be21ccc9846e5ef3dde0b5924de972ad",
+"/": "be21ccc9846e5ef3dde0b5924de972ad",
+"main.dart.js": "c8fe9e15ee9e6caec71037ba4666ea0b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
